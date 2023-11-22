fc75680273827ba4ccaec5a13299f0a542fdb3d5
blazejkrzak <blazej@m8b.pl>
2023-11-22 18:04:44 +0100

    Relayer extended with pessimistic approach about gas estimation in chargeback

--- src/modules/quota/quota.controller.ts
+++ src/modules/quota/quota.controller.ts
@@ -24,20 +24,4 @@ quotaController.post(
   }
 );
 
-//  This controller is only for debugging purposes and will be removed
-quotaController.get("/", async (req: Request, res: Response) => {
-  try {
-    req.body = {
-      address: "0x30E0693E4C4807C157F8b2e1426a74930C426b25",
-      timestamp: Math.floor(new Date().getTime() / 1000),
-      signature: "0x30E0693E4C4807C157F8b2e1426a74930C426b25",
-    };
-
-    const quota = await handleQuotas(req, quotaMode);
-    res.send(quota);
-  } catch (error) {
-    res.status(httpStatus.INTERNAL_SERVER_ERROR).send("Internal Server Error");
-  }
-});
-
 export default quotaController;
